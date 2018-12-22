17efd1f6c7765674b83c2e5b05a075389143015a
Pramod Chandra S <spramodchandra@gmail.com>
2018-12-21 17:49:35 -0800

    Add files via upload

---
+++ QLearning.py
@@ -0,0 +1,134 @@
+import numpy as np
+import transitionMatrix as tm
+import valueiteration as vi
+
+def ReadInput(filename):
+    obstacles=[]
+    with open(filename) as fp:
+        s=int(fp.readline())
+        o=int(fp.readline())
+        for i in range(o):
+            obstacles.append(tuple(reversed(list(map(int,fp.readline().strip().split(','))))))
+        cars_start=tuple(reversed(list(map(int,fp.readline().strip().split(',')))))
+        cars_end=tuple(reversed(list(map(int,fp.readline().strip().split(',')))))
+    return s,obstacles,cars_start,cars_end
+
+def Turn_Left(move):
+    if move==0:
+        return 3
+    if move==1:
+        return 2
+    if move==2:
+        return 0
+    if move==3:
+        return 1
+    return -1
+
+def Turn_Right(move):
+    if move==0:
+        return 2
+    if move==1:
+        return 3
+    if move==2:
+        return 1
+    if move==3:
+        return 0
+    return -1
+
+def UpdatePos(size,pos,move):
+    if move==0:
+        if pos[0]==0:
+            return pos
+        return pos[0]-1,pos[1]
+    if move==1:
+        if pos[0]==size-1:
+            return pos
+        return pos[0]+1,pos[1]
+    if move==2:
+        if pos[1]==size-1:
+            return pos
+        return pos[0],pos[1]+1    
+    if move==3:
+        if pos[1]==0:
+            return pos
+        return pos[0],pos[1]-1
+    return pos
+
+def updateStateActionMatrix(stateActionMatrix,currentState,nextState,action,reward,alpha,gamma):
+    action=int(action)
+    q=stateActionMatrix[action,currentState[0],currentState[1]]
+    ma=-100000
+    for i in range(4):
+        if stateActionMatrix[i,nextState[0],nextState[1]] >ma:
+            ma=stateActionMatrix[i,nextState[0],nextState[1]]
+    stateActionMatrix[action,currentState[0],currentState[1]]= q + alpha*(reward+gamma*ma-q)
+    return stateActionMatrix
+
+def updatePolicy(policy, utilityMatrix, pos):
+    policy[pos] = np.argmax(utilityMatrix[:,pos[0],pos[1]])
+    return policy
+
+def main():
+    size,obstacles,car_start,car_end=ReadInput("input.txt")
+    
+    transitionMatrix=tm.GenerateTransitionMatrix(size)    
+    
+    rewardMatrix = np.full((size,size),-1.0)
+    for a in obstacles:
+        rewardMatrix[a]=-101.0
+    
+    transitionMatrix=tm.GenerateTransitionMatrix(size)
+    utilityMatrix,optimalPolicy=vi.GetOptimalPolicy(size,transitionMatrix,rewardMatrix.copy(),car_end)
+    
+    #generate suboptimal policy from optimal policy
+    suboptimalPolicy=np.zeros((size,size))
+    for i in range(size):
+        for j in range(size):
+            if np.random.rand()>0.8:
+                suboptimalPolicy[i][j]=np.random.randint(low=0, high=4)
+            else:
+                suboptimalPolicy[i][j]=optimalPolicy[i][j]
+    policy = np.random.randint(low=0, high=4, size=(size, size)).astype(np.int32)
+    policy[car_end]=0
+    rewardMatrix[car_end]+=100
+    stateActionMatrix = np.zeros((4,size,size))
+    stateActionMatrix[0,car_end[0],car_end[1]]=stateActionMatrix[1,car_end[0],car_end[1]]=stateActionMatrix[2,car_end[0],car_end[1]]=stateActionMatrix[3,car_end[0],car_end[1]]=rewardMatrix[car_end]
+    gamma=0.9
+    alpha=0.1
+    
+    #generates episodes and update online
+    for j in range(4000):
+        pos = tuple([np.random.randint(0, size), np.random.randint(0, size)])
+        #pos=car_start
+        dest=car_end
+        np.random.seed(j)
+        swerve = np.random.random_sample(10000000)
+        k=0
+        while pos!=dest:
+            action = suboptimalPolicy[pos]
+            move =suboptimalPolicy[pos]
+            if swerve[k] > 0.7:
+                if swerve[k] > 0.8:
+                    if swerve[k] > 0.9:
+                        move = Turn_Left(Turn_Left(move))
+                    else:
+                        move = Turn_Right(move)
+                else:
+                    move = Turn_Left(move)
+            k+=1
+            newpos=UpdatePos(size,pos,move)
+            stateActionMatrix = updateStateActionMatrix(stateActionMatrix,pos,newpos,action,rewardMatrix[pos],alpha,gamma)
+            policy=updatePolicy(policy,stateActionMatrix,pos)
+            pos=newpos
+    
+    print("Dynamic Programming Utility:")
+    print(utilityMatrix)
+    print("Sarsa Utility:")
+    print(stateActionMatrix)
+    print ("optimal policy")
+    print (optimalPolicy)
+    print ("learned policy")
+    print (policy)
+    
+if __name__ == "__main__":
+    main()
